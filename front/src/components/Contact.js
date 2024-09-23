0509385f0122bd67f84a1d0bb06422894af27cea
Oumaima Guesmi <109872908+oumaima809@users.noreply.github.com>
2024-09-23 18:21:58 +0100

    Update Contact.js

--- front/src/components/Contact.js
+++ front/src/components/Contact.js
@@ -24,7 +24,7 @@ function Contact(){
     const handleSubmit = async(e)=>{
         e.preventDefault();
         setButtonText("Sending...");
-        let response = await fetch("http://localhost:5000/contact",{
+        let response = await fetch("https://portfolio-backend-dodh.onrender.com/contact",{
             method : "POST",
             headers : {"Content-Type":"application/json;charset=utf-8"},
             body : JSON.stringify(formDetails)
